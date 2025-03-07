59dc1cef498b2951d9c2946f47a9c43c52a5f03d
MUHAMMAD WAQAS <muhammadwaqas.1564@gmail.com>
2025-03-07 16:44:31 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')  # Set UPLOAD_FOLDER here
